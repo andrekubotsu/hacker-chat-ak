832c0b502274bfca97b55f9ebd81aa53d98babf3
Andre Kubotsu <contato201@gmail.com>
2021-03-26 23:25:37 -0300

    aula 04 - deploy

--- hacker-chat/client/index.js
+++ hacker-chat/client/index.js
@@ -1,4 +1,14 @@
+#!/usr/bin/env node
+
 /*
+    chmod +x index.js -> transforma em executavel
+*/
+
+/*
+    hacker-chat\
+        --username andrekubotsu \
+        --room sala01
+
     node index.js \
         --username andre \
         --room sala01 \
